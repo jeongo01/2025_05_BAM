9bcfda61f3abff18bd7edafc60accd55b9508b20
jeongo01 <wjddh123456@gmail.com>
2025-08-28 19:13:51 +0900

    getDateStr()

--- src/Main.java
+++ src/Main.java
@@ -36,7 +36,7 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(lastArticleId, title, body);
+				Article article = new Article(lastArticleId, Util.getDateStr(), title, body);
 				
 				articles.add(article);
 				
@@ -51,10 +51,10 @@ public class Main {
 					continue;
 				}
 				
-				System.out.println("번호	/	제목");
+				System.out.println("번호	/	제목	/	작성일");
 				for(int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d	/	%s\n", article.id, article.title);
+					System.out.printf("%d	/	%s	/	%s\n", article.id, article.title, article.regDate);
 				}
 				
 			}
@@ -78,6 +78,7 @@ public class Main {
 				}
 				
 				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("번호 : %d\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 				
@@ -118,11 +119,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 	
-	Article(int id, String title, String body) {
+	Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
