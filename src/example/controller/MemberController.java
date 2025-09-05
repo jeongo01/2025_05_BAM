6133ade0a74bb82373a1b37737ff1d4a80d9e0b4
jeongo01 <wjddh123456@gmail.com>
2025-09-05 21:16:22 +0900

    MemberController, ArticleController, isLoginIdDupChk

---
+++ src/example/controller/MemberController.java
@@ -0,0 +1,99 @@
+package example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import example.dto.Member;
+import example.util.Util;
+
+public class MemberController {
+
+	private List<Member> members;
+	private int lastMemberId;
+	private Scanner sc;
+
+	public MemberController(Scanner sc) {
+		this.members = new ArrayList<>();
+		this.lastMemberId = 0;
+		this.sc = sc;
+	}
+
+	public void doJoin() {
+
+		lastMemberId++;
+
+		String loginId = null;
+
+		while (true) {
+			System.out.printf("아이디 : ");
+			loginId = sc.nextLine().trim();
+
+			if (loginId.length() == 0) {
+				System.out.println("로그인 아이디는 필수 입력정보입니다.");
+				continue;
+			}
+
+			if (isLoginIdDupChk(loginId)) {
+				System.out.printf("%s는 이미 사용중인아이디 입니다.\n", loginId);
+				continue;
+			}
+
+			System.out.printf("%s는 사용가능한 아이디 입니다.\n", loginId);
+			break;
+		}
+
+		String loginPw = null;
+
+		while (true) {
+			System.out.printf("비밀번호 : ");
+			loginPw = sc.nextLine().trim();
+
+			if (loginPw.length() == 0) {
+				System.out.println("비밀번호는 필수 입력정보입니다.");
+				continue;
+			}
+
+			System.out.printf("비밀번호 확인 : ");
+			String loginPwChk = sc.nextLine();
+
+			if (loginPw.equals(loginPwChk) == false) {
+				System.out.println("비밀번호를 다시입력해주세요.");
+				continue;
+			}
+			break;
+		}
+
+		String name = null;
+
+		while (true) {
+			System.out.printf("이름 : ");
+			name = sc.nextLine().trim();
+
+			if (name.length() == 0) {
+				System.out.println("아이디는 필수 입력정보입니다.");
+				continue;
+			}
+			break;
+
+		}
+
+		Member member = new Member(lastMemberId, Util.getDateStr(), loginId, loginPw, name);
+
+		this.members.add(member);
+
+		System.out.printf("%s 회원님이 가입되었습니다.\n", name);
+
+	}
+
+	private boolean isLoginIdDupChk(String loginId) {
+		
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
